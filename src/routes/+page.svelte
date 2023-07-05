111c9948ae5597019ae9ff6631e5addc2baba84f
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-07-05 17:35:24 +0200

    Add image optimization with vite-imagetools plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
+<script>
+	import galleryImage from '../assets/5th-avenue-gallery-by-ejgold.png?format=webp';
+</script>
+
 <svelte:head>
 	<title>5th Ave. Gallery</title>
 </svelte:head>
 
 <div class="container mx-auto p-8 space-y-8">
 	<h1 class="h1">Hello 5th Ave. Gallery</h1>
+	<img class="w-auto h-auto" src={galleryImage} alt="5th Ave. Gallery" />
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-	<section>
-		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
-		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
-		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
-	</section>
+	<div class="flex justify-center">
+		<section>
+			<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+			<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+			<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		</section>
+	</div>
 </div>
