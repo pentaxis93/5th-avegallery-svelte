59a84959d8a20d5b4b535658156f7e9d55b32901
Turbobot <turbobot@vercel.com>
2023-07-25 10:14:10 +0200

    feat(create-turbo): apply official-starter transform

--- src/routes/blog/[slug]/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="prose dark:prose-invert p-4">
-	<slot />
-</div>
--- src/routes/uses/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>5th Ave. Gallery - Uses</title>
-</svelte:head>
-
-<div class="prose dark:prose-invert">
-	<slot />
-</div>
